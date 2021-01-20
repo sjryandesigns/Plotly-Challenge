70c30643204485d937be10ff7f6da147194b0351
sjryandesigns <sjryandesigns@gmail.com>
2021-01-20 09:50:05 -0800

    initial notes

---
+++ static/js/app.js
@@ -0,0 +1,32 @@
+// Handler and listener for capturing user input
+
+
+
+// Update dashboard with user input
+
+
+// Initialize dashboard
+
+
+// Save data into variables
+
+
+
+// Plot Bar Chart
+
+
+
+
+// Plot Bubble Chart
+
+
+
+// Define error function
+
+
+// Display same metadata
+
+// Display key-value pair from metadata JSON object 
+
+
+// Plot Gauge chart
