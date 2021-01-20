fa48e97c904ebdc15b58f54095f54359e5ad1cc2
sjryandesigns <sjryandesigns@gmail.com>
2021-01-20 10:13:51 -0800

    checkpoint

--- static/js/app.js
+++ static/js/app.js
@@ -8,9 +8,35 @@
 // Initialize dashboard
 
 
+// Read in data and console log
+// Top 10 OTUs found in single ID >> sample_values(values), otu_ids(labels), otu_labels (hovertext)
+// BUbble chart >> otu_ids (x values, marker color), sample_values(y values & marker size), otu_labels (text values)
+// Sample metadata (demographic information) for each ID
+
+
+// Display key-value pair from metadata JSON object 
+
+d3.json("samples.json").then((sampleData)=>{
+    console.log(sampleData)
+
+    var sortedBySampleValues = sampleData.samples[0].sort((a,b) => )
+
+    var otu_ids = sampleData.samples[0].otu_ids;
+    console.log(otu_ids)
+
+    var otu_labels sampleData.samples
+
+
+
+}
+
+
+
+
+);
+
+
 // Save data into variables
-
-
 
 // Plot Bar Chart
 
@@ -27,6 +53,11 @@
 // Display same metadata
 
 // Display key-value pair from metadata JSON object 
+
+
+
+
+
 
 
 // Plot Gauge chart
