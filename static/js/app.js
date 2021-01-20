85300e2b88003765a104400876fb924983c19bd7
sjryandesigns <sjryandesigns@gmail.com>
2021-01-20 12:00:25 -0800

    bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -30,9 +30,9 @@
         // Save data into variables
         var otu_ids = sampleData.samples[0].otu_ids;
         console.log(otu_ids);
-        var otu_labels = sampleData.samples[0].otu_labels.slice(0,10).reverse();
+        var otu_labels = sampleData.samples[0].otu_labels;
         console.log(otu_labels);
-        var sampleValue = sampleData.samples[0].sample_values.slice(0,10).reverse();
+        var sampleValue = sampleData.samples[0].sample_values;
         console.log(sampleValue); 
     
         var topTenOTU = otu_ids.slice(0,10).reverse();
@@ -43,19 +43,19 @@
     
 
 // // Top 10 OTUs found in single ID >> sample_values(values), otu_ids(labels), otu_labels (hovertext)
-        var bartrace = {
-            x: sampleValue,
+        var barTrace = {
+            x: sampleValue.slice(0,10).reverse(),
             y: topTenLabels,
-            hovertext: otu_labels,
+            hovertext: otu_labels.slice(0,10).reverse(),
             type: "bar",
             orientation: "h",
         };
         
         // data
-        var bardata = [bartrace];
+        var barData = [barTrace];
         
         //  Create a layout, what else can you do here besides titles?
-        var layout = {
+        var barLayout = {
             title: "Top 10 OTU",
             // yaxis:{
             //     tickmode: "linear",
@@ -68,7 +68,39 @@
             }
         };
 
-        Plotly.newPlot("bar", bardata, layout)
+        Plotly.newPlot("bar", barData, barLayout)
+
+        // Plot Bubble Chart
+
+        var bubbleTrace = {
+            x: otu_ids,
+            y: sampleValue,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+                color: otu_ids,
+                size: sampleValue,
+            }
+        };
+    
+        var bubbleData = [bubbleTrace];
+    
+        var bubbleLayout = {
+            xaxis:{
+                title: "OTU ID"
+            },
+            showlegend: false,
+            height: 600,
+            width: 1000,
+        };
+        
+        Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+
+
+
+
+
+
 
     });
 
@@ -133,29 +165,7 @@
 
 
 
-// // Plot Bubble Chart
 
-// var trace1 = {
-//     x: [1, 2, 3, 4],
-//     y: [10, 11, 12, 13],
-//     text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-//     mode: 'markers',
-//     marker: {
-//       color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-//       size: [40, 60, 80, 100]
-//     }
-//   };
-  
-//   var data = [trace1];
-  
-//   var layout = {
-//     title: 'Bubble Chart Hover Text',
-//     showlegend: false,
-//     height: 600,
-//     width: 600
-//   };
-  
-//   Plotly.newPlot('myDiv', data, layout);
 
 
 // Define error function
